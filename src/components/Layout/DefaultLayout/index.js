435878b92e72ecf1235b11740262323674a0ede2
NgocTan <phamngoctan2016@gmail.com>
2023-03-31 23:28:05 +0700

    lS 96

--- src/components/Layout/DefaultLayout/index.js
+++ src/components/Layout/DefaultLayout/index.js
@@ -1,13 +1,17 @@
-import Header from './Home';
-import SideBar from './Sidebar';
+import Header from '~/components/Layout/components/Header';
+import styles from './DefaultLayout.scss';
+import classNames from 'classnames/bind';
+import Sidebar from './Sidebar';
+
+const cx = classNames.bind(styles);
 
 function DefaultLayout({ children }) {
     return (
-        <div>
+        <div className={cx('wrapper')}>
             <Header />
-            <div className="container">
-                <SideBar />
-                <div className="content">{children}</div>
+            <div className={cx('container')}>
+                <Sidebar />
+                <div className={cx('content')}>{children}</div>
             </div>
         </div>
     );
