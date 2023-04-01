3a4543c5c2a82ab1dfeb52fd53ba58ae928ea7ab
NgocTan <phamngoctan2016@gmail.com>
2023-04-01 16:09:45 +0700

    Lesson 98 build Header #2

---
+++ src/components/AccountItem/index.js
@@ -0,0 +1,23 @@
+import classNames from 'classnames/bind';
+import styles from './AccountItem.module.scss';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCheckCircle } from '@fortawesome/free-solid-svg-icons';
+
+const cx = classNames.bind(styles);
+
+function AccountItem() {
+    return (
+        <div className={cx('wrapper')}>
+            <img className={cx('avatar')} src="https://ngoctan2k1.github.io/MyHeart/img/0.jpg" alt="" />
+            <div className={cx('info')}>
+                <h4 className={cx('name')}>
+                    <span>Nguyen Van A</span>
+                    <FontAwesomeIcon className={cx('check')} icon={faCheckCircle} />
+                </h4>
+                <span className={cx('username')}>nguyenvana</span>
+            </div>
+        </div>
+    );
+}
+
+export default AccountItem;
